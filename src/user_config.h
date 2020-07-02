f62fbc97e9d007f1450bafa83a2b338cae329dc3
ds661309 <dmytro.shafranskyi@broadcom.com>
2020-07-02 10:38:04 +0200

    added user config file with test endpoint

---
+++ src/user_config.h
@@ -0,0 +1,16 @@
+#ifndef user_config_h
+#define user_config_h
+
+#ifndef WIFI_SERVER_AP_NAME
+  #define WIFI_SERVER_AP_NAME "WFT"
+#endif
+
+#ifndef WIFI_SERVER_AP_PASSWORD
+  #define WIFI_SERVER_AP_PASSWORD "1p6QC58igf25AQc1YAB36WLU4"
+#endif
+
+#define SERVER_HOST_NAME "esp_server"
+
+const int PROBING_FREQUENCY_IN_MS = 3000;
+
+#endif
