eb5d1abfddd55982fe346e0346216e687f09dda2
Dmitriy Shafranskiy <dmitriy.shafranskiy.git@gmail.com>
2020-10-02 15:25:50 +0200

    refactoring.

--- src/simulation/openwrt.cpp
+++ src/simulation/openwrt.cpp
@@ -4,10 +4,42 @@
 #include "reporting/reporting.h"
 
 #ifdef ESP32
-  #include "SPIFFS.h"
+#include "SPIFFS.h"
 #endif
 
 AsyncWebServer openWrtServer(OPENWRT_PORT);
+
+void rootRedirect(AsyncWebServerRequest *request)
+{
+  AsyncWebServerResponse *response = request->beginResponse(200);
+  request->redirect("/cgi-bin/luci");
+  request->send(response);
+}
+
+void loginRedirect(AsyncWebServerRequest *request)
+{
+  notifyAttackOccurred(request->client()->remoteIP().toString().c_str());
+
+  AsyncWebServerResponse *response = request->beginResponse(403);
+  request->redirect("/cgi-bin/luci/login");
+}
+
+void missingResourceRedirect(AsyncWebServerRequest *request)
+{
+  //THIS /en resource is not downloaded as well on real Openwrt.
+  AsyncWebServerResponse *response = request->beginResponse(403);
+  request->send(response);
+}
+
+void notFound(AsyncWebServerRequest *request)
+{
+  //TODO: Original 404 page contains URL as well. Need to match it.
+  notifyAttackOccurred(request->client()->remoteIP().toString().c_str());
+
+  AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/owrt/html/404.html", "text/html");
+  response->setCode(404);
+  request->send(response);
+}
 
 void serveOpenWRT()
 {
@@ -17,33 +49,10 @@ void serveOpenWRT()
   openWrtServer.serveStatic("/luci-static/bootstrap/", SPIFFS, "/owrt/css/");
   openWrtServer.serveStatic("/luci-static/resources/", SPIFFS, "/owrt/js/");
 
-  openWrtServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-      AsyncWebServerResponse *response = request->beginResponse(200);
-      request->redirect("/cgi-bin/luci");
-      request->send(response);
-  });
-
-  openWrtServer.on("/cgi-bin/luci", HTTP_POST, [](AsyncWebServerRequest *request) {
-      notifyAttackOccurred(request->client()->remoteIP().toString().c_str());
-
-      AsyncWebServerResponse *response = request->beginResponse(403);
-      request->redirect("/cgi-bin/luci/login");
-  });
-
-  //THIS /en resource is not downloaded as well on real Openwrt.
-  openWrtServer.on("/cgi-bin/luci/admin/translations/en", HTTP_ANY, [](AsyncWebServerRequest *request) {
-      AsyncWebServerResponse *response = request->beginResponse(403);
-      request->send(response);
-  });
-
-  //TODO: Original 404 page contains URL as well. Need to match it. 
-  openWrtServer.onNotFound([](AsyncWebServerRequest *request) {
-    notifyAttackOccurred(request->client()->remoteIP().toString().c_str());
-
-    AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/owrt/html/404.html", "text/html");
-    response->setCode(404);
-    request->send(response);
-  });
+  openWrtServer.on("/", HTTP_GET, rootRedirect);
+  openWrtServer.on("/cgi-bin/luci", HTTP_POST, loginRedirect);
+  openWrtServer.on("/cgi-bin/luci/admin/translations/en", HTTP_ANY, missingResourceRedirect);
+  openWrtServer.onNotFound(notFound);
 
   openWrtServer.begin();
 }
