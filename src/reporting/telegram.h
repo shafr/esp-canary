5f03bf5624b5898154b7c1fcf826618320f931c4
Dmitriy Shafranskiy <dmitriy.shafranskiy.git@gmail.com>
2021-08-17 10:53:42 +0200

    Version bump - pio 3.2.0, telegram, (#24)

--- src/reporting/telegram.h
+++ src/reporting/telegram.h
@@ -4,12 +4,13 @@
 
 #include <Arduino.h>
 #include "user_config.h"
-#include <AsyncTelegram.h>
+#include <time.h>
+#include <AsyncTelegram2.h>
 #include "reporting/reporting.h"
-
 
 #ifdef ESP32
 #include <WiFi.h>
+#include <WiFiClient.h>
 #endif
 
 #ifdef ESP8266
